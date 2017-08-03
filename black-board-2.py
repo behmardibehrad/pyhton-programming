5665d764529055172a73f381b9d16d75a01aba13
Behrad Behmardi <behmardibehrad@users.noreply.github.com>
2017-08-03 18:31:10 -0400

    Update black-board-2.py

--- black-board-2.py
+++ black-board-2.py
@@ -6,8 +6,8 @@ with Browser() as browser:
  
 
   browser.visit('https://mymasonportal.gmu.edu/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_66_1')
-  browser.fill('user_id', 'bbehmard')
-  browser.fill('password', 'Arshia.1985!')
+  browser.fill('user_id', '')
+  browser.fill('password', '')
   browser.find_by_id('entry-login').first.click()
   browser.click_link_by_partial_href('/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_230_1')
 
